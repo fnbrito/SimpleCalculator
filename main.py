ca6a0be2245ac5b621807034f36000376db3a320
Filipe Brito <Fbrito3808@conestogac.on.ca>
2021-03-01 23:42:27 -0300

    Version 0.1 working prototype

---
+++ main.py
@@ -0,0 +1,126 @@
+#
+# Project created by Filipe Brito - 01/mar/2021
+#
+# Simple GUI calculator using Tkinter
+#
+# This app was created to study the capabilities of Tkinter and
+# it is not to be used as a replacement for a calculator App.
+#
+from tkinter import *
+from enum import Enum
+
+Operation = Enum('Operation', 'add subtract multiply divide')
+Op = Operation.add
+memory = 0
+
+root = Tk()
+
+e = Entry(root, width=35, borderwidth=5)
+e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+
+def button_click(number):
+    if len(e.get()) <= 10:
+        current = e.get()
+        e.delete(0, END)
+        e.insert(0, str(current) + str(number))
+    return
+
+
+def button_clear():
+    e.delete(0, END)
+    pass
+
+
+def button_equal():
+    global Op
+    global memory
+    newnumber = float(e.get())
+
+    if Op is Operation.add:
+        e.insert(0, str(memory + newnumber))
+    elif Op is Operation.subtract:
+        e.insert(0, str(memory - newnumber))
+    elif Op is Operation.multiply:
+        e.insert(0, str(memory * newnumber))
+    elif Op is Operation.divide:
+        e.insert(0, str(memory / newnumber))
+    pass
+
+
+def button_add():
+    global Op
+    global memory
+    if e.get() != "":
+        Op = Operation.add
+        memory = float(e.get())
+        e.delete(0, END)
+
+
+def button_subtract():
+    global Op
+    global memory
+    if e.get() != "":
+        Op = Operation.subtract
+        memory = float(e.get())
+        e.delete(0, END)
+
+def button_multiply():
+    global Op
+    global memory
+    if e.get() != "":
+        Op = Operation.multiply
+        memory = float(e.get())
+        e.delete(0, END)
+
+def button_divide():
+    global Op
+    global memory
+    if e.get() != "":
+        Op = Operation.divide
+        memory = float(e.get())
+        e.delete(0, END)
+
+# Define the buttons
+button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
+button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
+button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
+button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
+button_4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
+button_5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
+button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
+button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
+button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
+button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
+
+button_equal = Button(root, text="=", padx=86, pady=20, command=button_equal)
+button_clear = Button(root, text="C", padx=86, pady=20, command=button_clear)
+
+button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
+button_subtract = Button(root, text="-", padx=40, pady=20, command=button_subtract)
+button_multiply = Button(root, text="*", padx=40, pady=20, command=button_multiply)
+button_divide = Button(root, text="/", padx=40, pady=20, command=button_divide)
+
+# Put buttons on the screen
+button_add.grid(row=5, column=0)
+button_subtract.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
+button_equal.grid(row=5, column=1, columnspan=2)
+button_clear.grid(row=4, column=1, columnspan=2)
+
+button_0.grid(row=4, column=0)
+
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+button_3.grid(row=3, column=2)
+
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
+
+root.mainloop()
